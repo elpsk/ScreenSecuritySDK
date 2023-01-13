b7a6e5ade67933adedfd8f2159de62464f72a94a
Alberto Pasca <alberto.pasca@pirelli.com>
2023-01-13 15:33:35 +0100

    first draft

--- Sources/ScreenSecuritySDK/Protocols/Observable.swift
+++ Sources/ScreenSecuritySDK/Protocols/Observable.swift
@@ -10,4 +10,5 @@ typealias ObservableResponse = ((Any?, Bool) -> Void)?
 @objc protocol Observable {
     func observe( _ completion: ObservableResponse )
     @objc optional func observe( type: Any, completion: ObservableResponse )
+    @objc optional func observeScreenshot( lockedView: UIView, completion: ObservableResponse )
 }
